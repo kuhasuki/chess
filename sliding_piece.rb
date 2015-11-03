d881f9c1874be35267eeb97042841468d4e2c647
App Academy Student <student@appacademy.io>
2015-11-02 17:48:57 -0800

    Refactor Sliding Piece module

--- sliding_piece.rb
+++ sliding_piece.rb
@@ -17,16 +17,20 @@ module SlidingPiece
   end
 
   def valid_move?(move)
-    if move.all? { |coord| coord.between?(0, 7) }
-      return false if @board[move].color == @color
-      return true
-    end
-    false
+    in_bound?(move) && !teammate?(move)
+  end
+
+  def teammate?(move)
+    @board[move].color == @color
+  end
+
+  def in_bound?(move)
+    move.all? { |coord| coord.between?(0, 7) }
   end
 
   def deltas
   end
-  
+
   def enemy?(move)
     @board[move].color != @color && @board[move].color != nil
   end
