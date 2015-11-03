e23f3e3cc0d62403229a343dfdfd1dfaa9a0ba68
App Academy Student <student@appacademy.io>
2015-11-02 17:52:42 -0800

    Refactor Stepping Piece module

--- stepping_piece.rb
+++ stepping_piece.rb
@@ -6,15 +6,23 @@ module SteppingPiece
     cur_x, cur_y = pos
     deltas.each do |(dx, dy)|
       new_pos = [cur_x + dx, cur_y + dy]
-      moves << new_pos if new_pos.all? { |coord| coord.between?(0, 7) }
+      moves << new_pos if valid_move?(new_pos)
     end
-    moves.select { |move| valid_for_me?(move) }
+    moves
+  end
+
+  def valid_move?(move)
+    in_bound?(move) && !teammate?(move)
+  end
+
+  def in_bound?(move)
+    move.all? { |coord| coord.between?(0, 7) }
+  end
+
+  def teammate?(move)
+    @board[move].color == @color
   end
 
   def deltas
-  end
-
-  def valid_for_me?(move)
-    @board[move].color != @color
   end
 end
