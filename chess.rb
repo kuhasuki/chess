b0df39098971dd06dc5090a9479dfba3d303bc7a
App Academy Student <student@appacademy.io>
2015-11-03 17:05:09 -0800

    finish human player class

--- chess.rb
+++ chess.rb
@@ -3,10 +3,22 @@ require_relative 'display'
 require_relative 'human_player'
 
 class ChessGame
-  def initialize(board, player1, player2)
+  attr_reader :display
+
+  def initialize(board, player1_type, player2_type)
     @board = board
     @display = Display.new(@board)
-    @current_player_color = :black
+    @player1 = create_player(player1_type, :black)
+    @player2 = create_player(player2_type, :white)
+    @current_player, @previous_player = @player1, @player2
+  end
+
+  def create_player(player_type, color)
+    if player_type == "human"
+      HumanPlayer.new(@board, @display, color)
+    elsif player_type == "computer"
+      ComputerPlayer.new(@board, @display, color)
+    end
   end
 
   def run
@@ -16,61 +28,24 @@ class ChessGame
       rotate_player!
     end
     @display.render
-    puts "Game over! #{@current_player_color.to_s.capitalize} loses!"
+    puts "Game over! #{@current_player.color.to_s.capitalize} loses!"
   end
 
   def play_turn
-    start, end_pos = nil, nil
-    until @board.valid_move?(start, end_pos)
-      start = prompt_pos
-      start = prompt_pos until piece_at?(start) && my_piece?(start)
-      end_pos = prompt_pos
-      end_pos = prompt_pos until end_pos != start
-    end
-
+    start, end_pos = @current_player.prompt
     @board.move(start, end_pos)
   end
 
-  def notify_check
-    puts "#{@current_player_color.to_s.capitalize} is in check!"
-  end
-
-  def prompt_pos
-    pos = nil
-    until pos
-      @display.render
-      puts
-      notify_check if @board.in_check?(@current_player_color)
-      puts "#{@current_player_color.to_s.capitalize}'s turn now."
-      pos = @display.get_input
-    end
-    pos
-  end
-
   def over?
-    @board.checkmate?(@current_player_color)
+    @board.checkmate?(@current_player.color)
   end
 
   def rotate_player!
-    if @current_player_color == :black
-      @current_player_color = :white
-    else
-      @current_player_color = :black
-    end
-  end
-
-  def piece_at?(pos)
-    @board[pos].class != NilPiece
-  end
-
-  def my_piece?(pos)
-    @board[pos].color == @current_player_color
+    @current_player = @current_player == @player1 ? @player2 : @player1
   end
 end
 
 
 b = Board.new
-p1 = HumanPlayer.new(b)
-p2 = HumanPlayer.new(b)
-c = ChessGame.new(b, p1, p2)
+c = ChessGame.new(b, "human", "human")
 c.run
--- human_player.rb
+++ human_player.rb
@@ -1,13 +1,45 @@
 require_relative 'board'
 
 class HumanPlayer
+  attr_reader :color
 
-  def initialize(board)
-    @board = board
+  def initialize(board, display, color = :black)
+    @board, @display, @color = board, display, color
   end
 
   def prompt
+    start, end_pos = nil, nil
+    until @board.valid_move?(start, end_pos)
+      start = human_input
+      start = human_input until piece_at?(start) && my_piece?(start)
+      end_pos = human_input
+      end_pos = human_input until end_pos != start
+    end
 
+    [start, end_pos]
   end
 
+  def human_input
+    pos = nil
+    until pos
+      @display.render
+      puts
+      notify_check if @board.in_check?(@color)
+      puts "#{@color.to_s.capitalize}'s turn now."
+      pos = @display.get_input
+    end
+    pos
+  end
+
+  def notify_check
+    puts "#{@color.to_s.capitalize} is in check!"
+  end
+
+  def piece_at?(pos)
+    @board[pos].class != NilPiece
+  end
+
+  def my_piece?(pos)
+    @board[pos].color == @color
+  end
 end
